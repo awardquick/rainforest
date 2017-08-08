2b6751caef60521125e56892eacd906ad6e58494
Alex Jean-Baptiste <alxjeanbaptiste@gmail.com>
2017-08-08 13:46:46 -0400

    Created User Login and then Started with Shopping Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
+  belongs_to :user
   validates :description, :name, presence: true
   validates :price_in_cents, numericality: {only_integer: true, greater_than: 0}
   has_many :reviews
+  has_many :purchased_products
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'sessions#new'
   resources :reviews
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  post '/products/:id/buy' => 'products#buy'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
